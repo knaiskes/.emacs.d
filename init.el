8774a844e0e15f702d89f41a957df3c39c4fc92d
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-01-02 11:57:31 +0200

    update package-selcted-packages

--- init.el
+++ init.el
@@ -51,7 +51,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (wombat)))
- '(package-selected-packages (quote (php-mode go-mode))))
+ '(package-selected-packages (quote (web-mode php-mode go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
