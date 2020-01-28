a7f2e6fea2e92a8fec3a2788e054fcb7aab6bed3
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-01-28 13:40:05 +0200

    Update selected-packages list - add haskell-mode

--- init.el
+++ init.el
@@ -51,7 +51,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (wombat)))
- '(package-selected-packages (quote (js2-mode markdown-mode web-mode php-mode go-mode))))
+ '(package-selected-packages
+   (quote
+    (haskell-mode js2-mode markdown-mode web-mode php-mode go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
