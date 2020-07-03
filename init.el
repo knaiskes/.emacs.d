e98288e78770e5fbdcbdbf53bc950fd05f7ad429
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-07-03 19:08:45 +0300

    Update package-selected-packages

--- init.el
+++ init.el
@@ -65,7 +65,7 @@
  '(display-line-numbers-type (quote relative))
  '(package-selected-packages
    (quote
-    (yaml-mode yasnippet haskell-mode js2-mode markdown-mode web-mode php-mode go-mode))))
+    (scala-mode yaml-mode yasnippet haskell-mode js2-mode markdown-mode web-mode php-mode go-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
