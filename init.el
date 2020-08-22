e8bc1976ad3153bb0942808b4b453970ae11eb57
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2020-08-22 16:30:57 +0300

    Add dockerfile-mode

--- init.el
+++ init.el
@@ -61,11 +61,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wombat)))
- '(display-line-numbers-type (quote relative))
+ '(custom-enabled-themes '(wombat))
+ '(display-line-numbers-type 'relative)
  '(package-selected-packages
-   (quote
-    (scala-mode yaml-mode yasnippet haskell-mode js2-mode markdown-mode web-mode php-mode go-mode))))
+   '(dockerfile-mode scala-mode yaml-mode yasnippet haskell-mode js2-mode markdown-mode web-mode php-mode go-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
