d4bad660e8e81cd71ae7cccd1a861e718526c462
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2021-09-06 19:00:55 +0300

    add dart-mode

--- init.el
+++ init.el
@@ -74,7 +74,7 @@
  '(custom-enabled-themes '(wombat))
  '(display-line-numbers-type 'relative)
  '(package-selected-packages
-   '(json-mode magit flycheck typescript-mode dockerfile-mode scala-mode yaml-mode yasnippet haskell-mode markdown-mode web-mode php-mode go-mode)))
+   '(dart-mode json-mode magit flycheck typescript-mode dockerfile-mode scala-mode yaml-mode yasnippet haskell-mode markdown-mode web-mode php-mode go-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
