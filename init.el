47c78cb69b7f23de9a05796caeb2c056aebd3711
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2022-08-20 03:23:57 +0300

    Add rust-mode

--- init.el
+++ init.el
@@ -81,10 +81,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(modus-vivendi))
-
  '(display-line-numbers-type 'relative)
  '(package-selected-packages
-   '(lsp-mode company json-mode magit flycheck typescript-mode dockerfile-mode yaml-mode yasnippet markdown-mode web-mode go-mode)))
+   '(rust-mode lsp-mode company json-mode magit flycheck typescript-mode dockerfile-mode yaml-mode yasnippet markdown-mode web-mode go-mode)))
 
 (package-initialize)
 (unless package-archive-contents
@@ -141,5 +140,12 @@
 ;; lsp-mode
 (require 'lsp-mode)
 (add-hook 'js-mode-hook #'lsp)
+(add-hook 'python-mode-hook #'lsp)
 (setq lsp-keymap-prefix "C-c l")
 (define-key lsp-mode-map (kbd "C-c l") lsp-command-map)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
