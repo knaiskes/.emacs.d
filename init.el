122cff57ccf909f8afb3d6e9f033962763757745
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-07-30 20:43:31 +0300

    Add eslint function in order to load the config

--- init.el
+++ init.el
@@ -153,3 +153,17 @@
 (setq-default fill-column 80)
 
 (setq isearch-lazy-count t)
+;;(setq flycheck-javascript-eslint-executable "~/programming/test-eslint-ci/node_modules/.bin/eslint")
+
+(defun my/use-eslint-from-node-modules ()
+  "Search for ESLint locally in node_modules."
+  (let* ((root (locate-dominating-file
+           (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint
+          (and root (expand-file-name "node_modules/.bin/eslint" root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+
+;;(add-hook 'flycheck-mode-hook #'my/use-eslint-from-node-modules)
