01c82ed2cbe078ddb3f67c3902b00ebc7b43d60a
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2023-09-17 13:14:34 +0300

    Add gomod reference in treesit-language-source-alist

--- init.el
+++ init.el
@@ -95,6 +95,7 @@
   :init
   (setq treesit-language-source-alist
    '((go . ("https://github.com/tree-sitter/tree-sitter-go"))
+     (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
      (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
      (json . ("https://github.com/tree-sitter/tree-sitter-json"))
      (python . ("https://github.com/tree-sitter/tree-sitter-python"))
