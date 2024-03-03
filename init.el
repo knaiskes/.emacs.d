aba8c989252716762ffec32dddcc50e0529274ed
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2024-03-03 18:11:12 +0200

    Set tramp history file to nil and make bash as the default shell

--- init.el
+++ init.el
@@ -194,3 +194,6 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(setq tramp-histfile-override nil)
+(setq explicit-shell-file-name "/bin/bash")
